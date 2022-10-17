cac4d58a3afbf8d55e53235e69f0f16773f96378
juovani kirlos <j1kirlos@student.bridgew.edu>
2022-10-17 18:51:01 -0400

    wrote some code

--- cards.py
+++ cards.py
@@ -1,2 +1,22 @@
 import random
 
+
+class Counter:
+    number = None
+
+    def __init__(self, numbers):
+        self.number = numbers
+
+
+class Color:
+    type = None
+
+    def __init__(self, type_color):
+        self.type = type_color
+
+
+class Cards:
+
+    def __str__(self):
+        return 'color: {}, type: {}, number{}'.format(
+        )
