d03be4b2d20935023965ee9a0b7c6790636ac075
juovani kirlos <j1kirlos@student.bridgew.edu>
2022-10-25 12:52:01 -0400

    wrote some code

--- cards.py
+++ cards.py
@@ -1,22 +1,37 @@
 import random
 
 
-class Counter:
-    number = None
-
-    def __init__(self, numbers):
-        self.number = numbers
+class Suit:
+    def __init__(self, suit_name):
+        self.suit_name = suit_name
 
 
-class Color:
-    type = None
+class Rank:
+    def __init__(self, rank_value, icon):
+        self.rank_value = rank_value
+        self.icon = icon
 
-    def __init__(self, type_color):
-        self.type = type_color
 
-
-class Cards:
+class Card:
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
 
     def __str__(self):
-        return 'color: {}, type: {}, number{}'.format(
-        )
+        return '{}{}'. format(self.rank.icon, self.suit.suit_name)
+
+
+this_cards_suit = Suit("D")
+this_cards_rank = Suit(5, "5")
+this_card = Card(this_cards_rank, this_cards_suit)
+print(this_card)
+
+
+
+
+
+
+
+
+
+
